51906134ed2338c15b6460e91045eff56a7db311
suzuki <suzutaku3215@outlook.jp>
2025-10-05 23:03:58 +0900

    ぬるぬる

---
+++ helloSpringBoot/src/main/java/com/example/demo/controller/HelloSpringBootWebController.java
@@ -0,0 +1,22 @@
+package com.example.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class HelloSpringBootWebController {
+	@RequestMapping(value="/", method=RequestMethod.GET)
+    public String index() {
+        return "index";
+    }
+
+    @RequestMapping(value="/result", method=RequestMethod.POST)
+    public String send(@RequestParam("inputvalue")String inputvalue, Model model) {
+        model.addAttribute("message", inputvalue);
+        return "result";
+    }
+
+}
